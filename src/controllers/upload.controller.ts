d0186e7aff44e37b1bafc99241fca43efbbac8d1
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2026-01-05 19:17:12 +0900

    Feat(upload-image): 오디오 파일 저장 방식 변경 ## Feat - 오디오 파일을 받을 때 multer 미들 웨어에서 파일 이름의 공백을 언더바 ( _ ) 로 치환하여 저장 - 치환하여 저장된 filename을 받아 mime type 제거 및 json 파일 genrate

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -8,13 +8,10 @@ export function renderPage(_: Request, res: Response) {
 }
 
 export async function generateFile(req: Request, res: Response) {
-  const file = req.file;
-  const name = file!.originalname;
+  const name = req.file!.filename;
 
   try {
-    const origin = replace(name, "", "mime");
-    const toSave = replace(origin, "_", "space");
-    await generateJSON(origin, toSave.toLowerCase());
+    await generateJSON(replace(name, "", "mime"));
     res.sendStatus(200);
   } catch (e) {
     res.status(500).json({ message: "파일 업로드에 실패하였습니다." });
