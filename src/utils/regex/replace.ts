955ed6f687346e83ca8b1978de3ea135824c9008
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2026-01-06 19:15:38 +0900

    Feat(utils): regex.replaceAll 함수 추가 ## Feat - types로 주어진 regex 목록들을 한번에 처리할 수 있는 함수 추가

--- src/utils/regex/replace.ts
+++ src/utils/regex/replace.ts
@@ -1,8 +1,16 @@
 const REGEX = {
   space: /\s/g,
   mime: /.wav/g,
-};
+} as const;
 
-export function replace(origin: string, to: string, type: "space" | "mime") {
+export function replace(origin: string, to: string, type: keyof typeof REGEX) {
   return origin.replace(REGEX[type], to);
+}
+
+export function replaceAll(origin: string, to: string, types: (keyof typeof REGEX)[]) {
+  for (let type of types) {
+    origin = origin.replace(REGEX[type], to);
+  }
+
+  return origin;
 }
